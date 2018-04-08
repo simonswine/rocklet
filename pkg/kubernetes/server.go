8434f99f4c87f7f0665c3af638e79fd223a17ff5
Christian Simon <simon@swine.de>
2018-04-08 15:02:27 +0100

    Make command forwarding work

--- pkg/kubernetes/server.go
+++ pkg/kubernetes/server.go
@@ -2,19 +2,79 @@ package kubernetes
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os/exec"
+	"time"
+
+	"github.com/kr/pty"
+	"k8s.io/apimachinery/pkg/types"
+	remotecommandconsts "k8s.io/apimachinery/pkg/util/remotecommand"
+	"k8s.io/client-go/tools/remotecommand"
+	remotecommandserver "k8s.io/kubernetes/pkg/kubelet/server/remotecommand"
 )
 
-func (k *Kubernetes) helloServer(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example server.\n"))
-	k.logger.Debug().Interface("headers", req.Header).Str("path", req.RequestURI).Msg("server request unhandled")
-	// fmt.Fprintf(w, "This is an example server.\n")
-	// io.WriteString(w, "This is an example server.\n")
+func (k *Kubernetes) ExecInContainer(name string, uid types.UID, container string, cmd []string, stdin io.Reader, stdout, stderr io.WriteCloser, tty bool, resize <-chan remotecommand.TerminalSize, timeout time.Duration) error {
+
+	// Create arbitrary command.
+	c := exec.Command("bash")
+
+	// Start the command with a pty.
+	ptmx, err := pty.Start(c)
+	if err != nil {
+		return err
+	}
+	// Make sure to close the pty at the end.
+	defer func() { _ = ptmx.Close() }() // Best effort.
+
+	// Handle pty size.
+	go func() {
+		for s := range resize {
+			err := pty.Setsize(ptmx, &pty.Winsize{
+				Cols: s.Width,
+				Rows: s.Height,
+			})
+			if err != nil {
+				k.logger.Warn().Msgf("error resizing pty: %s", err)
+			}
+		}
+	}()
+
+	// Copy stdin to the pty and the pty to stdout.
+	go func() { _, _ = io.Copy(ptmx, stdin) }()
+	_, _ = io.Copy(stdout, ptmx)
+
+	stderr.Close()
+	stdout.Close()
+
+	return nil
+}
+
+func (k *Kubernetes) remoteControlHandle(w http.ResponseWriter, req *http.Request) {
+	streamOpts, err := remotecommandserver.NewOptions(req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	remotecommandserver.ServeExec(
+		w,
+		req,
+		k,
+		k.fakeRemoteControlPodName(),
+		types.UID("x"),
+		"container",
+		[]string{"cmd"},
+		streamOpts,
+		time.Minute,
+		remotecommandconsts.DefaultStreamCreationTimeout,
+		remotecommandconsts.SupportedStreamingProtocols,
+	)
 }
 
 func (k *Kubernetes) runServer() {
-	http.HandleFunc("/", k.helloServer)
+	http.HandleFunc("/exec/default/remote-control-rockrobo/container", k.remoteControlHandle)
 	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", k.flags.Kubernetes.KubeletPort), k.flags.Kubernetes.CertPath, k.flags.Kubernetes.KeyPath, nil)
 	if err != nil {
 		k.logger.Fatal().Err(err).Msg("ListenAndServe")
